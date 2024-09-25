6f2dd08d0b0b0de2bfea584fe94688649f955e84
CELIO LAZARO PEREIRA <celio.lpereira@senacsp.edu.br>
2024-09-25 14:33:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Solicitar qu o usuário insira a idade
+let idade = parseInt (prompt("Digite a sua idade"));
+// Se a idade for maior que 18, o usuário poderá acessar o sistema
+if (idade >=18){
+    alert ("Acesso permitido!😉");
+}else{
+    alert ("Acesso não permitido😢")
+}
